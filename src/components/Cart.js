0854de751774ae79db43811730d95bfd121819fe
saba <sabam1015@gmail.com>
2021-08-24 11:49:44 -0300

    error en carrito

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import './cart.css'
+import Waffle from './Waffles';
+
 import Waffles from './Waffles';
 
-const Cart = () => {
+const Cart = ({cart, setCart}) => {
     return (
-        <div>
+        <div className = 'cart'>
             <h3>Carrito</h3>
+
+            {Cart.length === 0 ? (
+          <p>No hay nada por aquí...</p>
+        ) : (
+          Cart.map((waffle) => (
+            <Waffle
+              key={waffle.id}
+              waffle={waffle}
+              cart={cart}
+              setCart={setCart}
+            />
+          ))
+        )}
         </div>
-    );
+    )
 };
 
 export default Cart;
