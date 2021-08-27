992c117c2178bfd75e5820729c46fda013bccfcd
saba <sabam1015@gmail.com>
2021-08-27 00:32:03 -0300

    empezando navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,17 +5,25 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
+import logo from '../images/logo.png'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginBottom: "7rem",
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  AppBar:{
+    backgroundColor:"#9b6233",
+    boxShadow: "none",
   },
-  title: {
-    flexGrow: 1,
+  grow:{
+    flexGrow:1,
+  },
+  button: {
+    marginLeft: theme.spacing(2),
+  },
+  image: {
+    marginRight: "10px",
+    height: "3rem",
   },
 }));
 
@@ -24,13 +32,13 @@ export default function Navbar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <img src={logo} className={classes.image}/>
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
+          <Typography variant="h6" color="textPrimary" component="p">
+            Bienvenido!
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
